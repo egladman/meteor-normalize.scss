85a16fda8f76429b829de863bceabb4090efdb5f
Steve Zhu <st.zhu1@gmail.com>
2014-09-01 14:36:39 -0400

    Initial commit

---
+++ package.js
@@ -0,0 +1,5 @@
+Package.describe({
+	summary: "A collection of HTML element and attribute style-normalizations.",
+	version: "1.0.0",
+	git: "https://github.com/stevezhu/meteor-normalize.scss.git"
+});
