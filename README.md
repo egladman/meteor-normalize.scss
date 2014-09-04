0955cbefc03325b4d00006a8ed4a65e15a89f648
Steve Zhu <st.zhu1@gmail.com>
2014-09-04 18:22:05 -0400

    Using sass package

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 A meteor package for [normalize.css](http://necolas.github.io/normalize.css/).
+
+## Usage
+
+`meteor add stevezhu:sass` then `@import "normalize"`
--- package.js
+++ package.js
@@ -1,5 +1,11 @@
 Package.describe({
 	summary: "A collection of HTML element and attribute style-normalizations.",
-	version: "1.0.0",
+	version: "1.1.1",
 	git: "https://github.com/stevezhu/meteor-normalize.scss.git"
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom("METEOR@0.9.0");
+	api.use('stevezhu:sass@1.2.1');
+	api.addFiles('sass_include_paths.json', 'server');
 });
