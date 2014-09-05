b86e39a925f19944884b5a9af6f9aae2e9dc766c
Steve Zhu <st.zhu1@gmail.com>
2014-09-05 14:07:20 -0400

    Bump sass version and travis ci build only master

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
 	summary: "A collection of HTML element and attribute style-normalizations.",
-	version: "1.1.1",
+	version: "1.1.2",
 	git: "https://github.com/stevezhu/meteor-normalize.scss.git"
 });
 
 Package.onUse(function(api) {
 	api.versionsFrom("METEOR@0.9.0");
-	api.use('stevezhu:sass@1.2.1');
+	api.use('stevezhu:sass@1.2.2');
 	api.addFiles('sass_include_paths.json', 'server');
 });
