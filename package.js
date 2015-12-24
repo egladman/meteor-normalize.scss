892844710a23b5fb731191300d4d4a8105e07ef4
Eli Gladman <eli.gladman@gmail.com>
2015-12-24 10:46:36 -0500

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: "A collection of HTML element and attribute style-normalizations.",
-	version: "1.1.2",
+	version: "1.1.3",
 	git: "https://github.com/egladman/meteor-normalize.scss.git"
 });
 
